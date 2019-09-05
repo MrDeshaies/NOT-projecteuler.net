486567aa9d3c48087e33785c0f512081f91b76b9
Benoit Deshaies <benoit.deshaies@gmail.com>
2019-09-05 13:27:26 -0400

    solved 026

--- euler_026.py
+++ euler_026.py
@@ -17,28 +17,67 @@
 
 import math
 
-class UnitDivision:
+NO_CYCLE = -1
+
+class UnitDivision:    
     def __init__(self,divisor):
         self.term = 10
         self.divisor = divisor
         self.answer = []
+        self.termsSeen = []
+        self.cycleStart = NO_CYCLE
     
     def borrow(self):
         self.term *= 10
         self.answer.append(0)
     
+    def isCycleDetected(self):
+        if self.term in self.termsSeen:
+            self.cycleStart = self.termsSeen.index(self.term)
+            return True
+        self.termsSeen.append(self.term)
+        return False
+    
+    def cycleLength(self):
+        if self.cycleStart == NO_CYCLE:
+            return 0
+        return len(self.answer) - self.cycleStart
+    
     def divide(self):
         while self.term != 0:
             while self.term < self.divisor:
                 self.borrow()
+            if (self.isCycleDetected()):
+                break
             f = math.floor(self.term/self.divisor)
             self.term = self.term - (self.divisor * f)
             self.term *= 10
             self.answer.append(f)
     
     def printAnswer(self):
-        print("0." + str("".join([str(x) for x in self.answer])))
+        if self.cycleStart == NO_CYCLE:
+            print("1/" + str(self.divisor) + " = 0." + str("".join([str(x) for x in self.answer])))
+        else:
+            answer = "1/" + str(self.divisor) + " = 0."
+            answer += str("".join([str(x) for x in self.answer[:self.cycleStart]]))
+            answer += "("
+            answer += str("".join([str(x) for x in self.answer[self.cycleStart:]]))
+            answer += ")\t" + str(self.cycleLength())
+            print(answer)
 
-x = UnitDivision(8)
+
+# Find the value of d < 1000 for which 1/d contains the longest recurring cycle
+maxCycleLength = 0
+maxDivisor = 0
+for i in range(1,1000):
+    x = UnitDivision(i)
+    x.divide()
+    if x.cycleLength() > maxCycleLength:
+        maxCycleLength = x.cycleLength()
+        maxDivisor = i
+    x.printAnswer()
+
+print("And the winner is...")
+x = UnitDivision(maxDivisor)
 x.divide()
 x.printAnswer()

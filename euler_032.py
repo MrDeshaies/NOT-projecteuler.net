5b063bbe502727e651cf81f50219ff7b73679549
Benoit Deshaies <benoit.deshaies@gmail.com>
2019-09-21 09:59:46 -0400

    solved 032

--- euler_032.py
+++ euler_032.py
@@ -9,3 +9,27 @@
 #
 # HINT: Some products can be obtained in more than one way so be sure to only include it once 
 # in your sum.
+
+import itertools
+
+products_found = set()
+
+def find_product(digits):
+    # digits is an array of digits 1-9 in whatever order. Find two positions, p and e,
+    # 0 < p < e, to break the string such that digits[0:p] x digits[p:e] = digits[e:]
+    # 'p' = product position, 'e' = equal position
+
+    for p in range(1,8): # max of 7 leaves 2 digits at the end, one for multiplier, one for product
+        for e in range(p+1,9):
+            multiplicand = int("".join(digits[0:p]))
+            multiplier = int("".join(digits[p:e]))
+            product = int("".join(digits[e:]))
+            if multiplicand * multiplier == product:
+                if product in products_found:
+                    continue
+                print("{0} = {1} x {2}".format(product,multiplicand,multiplier))
+                products_found.add(product)
+
+for i in itertools.permutations([str(x) for x in range(1,10)]):
+    find_product(i)
+print(sum(products_found))

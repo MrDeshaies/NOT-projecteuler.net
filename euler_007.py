fdc5bd4834e8e44858fdde08df2c1c4503c62536
Benoit Deshaies <benoit.deshaies@gmail.com>
2019-10-06 14:40:46 -0400

    doubled the speed of 007, lol

--- euler_007.py
+++ euler_007.py
@@ -1,13 +1,13 @@
 import math
-from euler import *
+from euler import isPrime
 
 # What is the 10,001st prime number?
-primesFound = 0
-x = 2
+primesFound = 1
+x = 3
 while True:
     if isPrime(x):
         primesFound += 1
     if primesFound == 10001:
         print(x)
         break
-    x += 1
+    x += 2

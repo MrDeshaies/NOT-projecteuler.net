ca78bb520e9714f891c13452617d62bccd0e7837
Benoit Deshaies <benoit.deshaies@gmail.com>
2019-10-14 16:15:22 -0400

    solved 048 (easy)

---
+++ euler_048.py
@@ -0,0 +1,8 @@
+# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+UPPER_LIMIT = 1000
+total = 0
+for i in range(1,UPPER_LIMIT+1):
+    total += i**i
+print(str(total)[-10:])

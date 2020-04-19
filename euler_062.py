1d67e7ad21ea684a105efe767a9d4a39faaeeee3
Benoit Deshaies <benoit.deshaies@gmail.com>
2020-04-19 19:30:07 -0400

    fixed double calculating c

--- euler_062.py
+++ euler_062.py
@@ -17,17 +17,16 @@ def is_permutation(x,y):
             return False
     return True
 
-def find_cube_with_permutations(number_of_permutations):
+def find_cube_with_number_of_permutations(number_of_permutations):
     all_cubes = [i**3 for i in range(10000)]
     for i,c in enumerate(all_cubes):
-        c = i**3
         print("Testing {} with value {}".format(i,c))
         cubic_permutations = find_cubic_permutations(all_cubes,c)
         if len(cubic_permutations) == number_of_permutations:
             return c
 
 def find_cubic_permutations(all_cubes, number):
-    # goes through the cubes, and finds those are are digit permutation of number.
+    # goes through the cubes, and finds those that are are digit permutation of number.
     # to avoid having to go through all of them, we stop once we reach a cube whose
     # length is greater than our number. Assumes all_cubes is sorted
     cubic_permutations = []
@@ -40,5 +39,5 @@ def find_cubic_permutations(all_cubes, number):
     return cubic_permutations
 
 if __name__ == '__main__':
-    smallest_cube = find_cube_with_permutations(5)
+    smallest_cube = find_cube_with_number_of_permutations(5)
     print(smallest_cube)

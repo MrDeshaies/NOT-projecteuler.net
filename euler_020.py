53613770c02b9a70d6b252471d4815d09b2bc292
Benoit Deshaies <benoit.deshaies@gmail.com>
2020-04-28 20:54:30 -0400

    simplify 020; too much code

--- euler_020.py
+++ euler_020.py
@@ -3,7 +3,4 @@ import math
 # Find the sum of the digits in the number 100!
 
 number = math.factorial(100)
-sumOfDigits = 0
-for digit in str(number):
-    sumOfDigits += int(digit)
-print(sumOfDigits)
+print( sum([int(d) for d in str(number)]) )

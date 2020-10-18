12f7799fefa1e9ba4822e056152164b956192c6b
Benoit Deshaies <benoit.deshaies@gmail.com>
2020-10-18 16:35:32 -0400

    solved 065

---
+++ euler_065.py
@@ -0,0 +1,35 @@
+# Euler 65
+# convergents for e are [2; 1,2,1,  1,4,1,  1,6,1,  1,8,1, ... 1,2k,1]
+# So the first 10 terms are 2, 3, 8/3, 11/4, 19/7, 82/32 ... 1457/536.
+# Find the sum of digits in the numerator of the 100th convergent 
+# of the continued fraction for e.
+
+from fractions import Fraction
+from math import ceil
+
+def sum_digits(x):
+    return sum([int(d) for d in str(x)])
+
+def generate_convergents_for_e(n):
+    result = [2]
+    groups = ceil((n-1)/3)
+    for k in range(1,groups+1):
+        result += [1,2*k,1]
+    return result[0:n]
+
+def convert_convergents_to_fraction(partial_denominators):
+    if len(partial_denominators) == 1:
+        return Fraction(partial_denominators[0])
+    
+    # build the fraction bottom-up
+    result = Fraction(partial_denominators[-1])
+    for term in partial_denominators[-2::-1]:
+        result = Fraction(term) + Fraction(1,result)
+    return result
+
+if __name__ == '__main__':
+    for i in [100]:#range(1,11):
+        convergents = generate_convergents_for_e(i)
+        convergent_fraction = convert_convergents_to_fraction(convergents)
+        sum_digits_numerator = sum_digits(convergent_fraction.numerator)
+        print("{}: {} (sum digits numerator: {})".format(i,convergent_fraction, sum_digits_numerator))

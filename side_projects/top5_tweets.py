5a3a71c105710dc2b7a2baf56dbac47b2803e1f9
Benoit Deshaies <benoit.deshaies@gmail.com>
2022-04-20 12:43:49 -0400

    also find top retweets

--- side_projects/top5_tweets.py
+++ side_projects/top5_tweets.py
@@ -23,11 +23,24 @@ user_id = get_user_id(client, USERNAME)
 # get the tweets
 tweets = tw.Paginator(client.get_users_tweets, id=user_id,
     tweet_fields=['id','public_metrics','created_at','text'],
-    max_results=100).flatten(limit=5000)
-tweet_data = [[tweet.id, tweet.public_metrics['like_count'], tweet.created_at, tweet.text] for tweet in tweets]
+    exclude='retweets', max_results=100).flatten(limit=5000)
+tweet_data = [[
+    tweet.id, 
+    tweet.public_metrics['like_count'], 
+    tweet.public_metrics['retweet_count'], 
+    tweet.public_metrics['quote_count'], 
+    tweet.created_at, tweet.text]
+    for tweet in tweets]
 
 # use pandas to get the top 5
-tweet_frame = pd.DataFrame(data=tweet_data, columns=['id', 'likes', 'created_at', 'text'])
+tweet_frame = pd.DataFrame(data=tweet_data, columns=['id', 'likes', 'retweets', 'quotes', 'created_at', 'text'])
 tweet_frame['created_date'] = tweet_frame['created_at'].dt.date
-top_5 = tweet_frame.sort_values(by='likes', ascending=False)[:5][['likes', 'created_date', 'text']]
-print(top_5)
+tweet_frame['total_retweets'] = tweet_frame['retweets'] + tweet_frame['quotes']
+
+print("=== Top likes ===")
+top_posts = tweet_frame.sort_values(by='likes', ascending=False)[:10][['likes', 'created_date', 'text']]
+print(top_posts)
+
+print("=== Top retweets ===")
+top_posts = tweet_frame.sort_values(by='total_retweets', ascending=False)[:10][['total_retweets', 'created_date', 'text']]
+print(top_posts)

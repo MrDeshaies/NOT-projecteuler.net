cafa2d9dd38231dd6dbba1b72b8aab31ed62c6ab
Benoit Deshaies <benoit.deshaies@gmail.com>
2022-04-25 20:44:58 -0400

    this simpler version runs faster :'-(

--- side_projects/birthday_paradox.py
+++ side_projects/birthday_paradox.py
@@ -11,11 +11,7 @@ NUMBER_OF_MATCH = 0
 
 def has_duplicate(l) -> bool:
     """return a boolean, whether the array contains a duplicate element"""
-    for i in range(len(l)-1):
-        for j in range(i+1, len(l)):
-            if l[i] == l[j]:
-                return True
-    return False
+    return len(l) != len(set(l))
 
 for i in range(NUMBER_OF_SETS):
     # represent birthdate as int between 0 and 365
@@ -23,4 +19,5 @@ for i in range(NUMBER_OF_SETS):
     if has_duplicate(bithday_list):
         NUMBER_OF_MATCH += 1
 
-print("Ratio with {} people is {}".format(NUMBER_OF_PEOPLE, (NUMBER_OF_MATCH/NUMBER_OF_SETS)))
+print("Ratio with {} people is {}"
+    .format(NUMBER_OF_PEOPLE, (NUMBER_OF_MATCH/NUMBER_OF_SETS)))
